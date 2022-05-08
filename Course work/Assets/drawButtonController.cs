e493dcdd3d166b475be1781b33f0068e04087848
KryzPetr1533 <62902540+KryzPetr1533@users.noreply.github.com>
2022-05-08 15:34:34 +0300

    Colorising GUI
    UI got new colors. Some dimentions changed.

--- Course work/Assets/drawButtonController.cs
+++ Course work/Assets/drawButtonController.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class drawButtonController : MonoBehaviour
 {
     private bool _drawingStatus = false;
-
+    public Color previousColor;
     public bool GetDrawingStatus()
     {
         return _drawingStatus;
     }
 
-    public void OnClick()
+    public void OnClick(Button drawButton)
     {
         _drawingStatus = !_drawingStatus;
+        if(_drawingStatus)
+        {
+            previousColor = drawButton.GetComponent<Image>().color;
+            drawButton.GetComponent<Image>().color = Color.red;
+        } else
+        {
+            drawButton.GetComponent<Image>().color = previousColor; 
+        }
     }
 }
