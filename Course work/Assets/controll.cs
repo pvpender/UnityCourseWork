ce635437844b458fe06bf82a438f013ee51d2299
KryzPetr1533 <62902540+KryzPetr1533@users.noreply.github.com>
2022-05-14 10:43:29 +0300

    Creating  settings button

--- Course work/Assets/controll.cs
+++ Course work/Assets/controll.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class controll : MonoBehaviour
 {
@@ -20,6 +21,6 @@ public class controll : MonoBehaviour
     }
     void Update()
     {
-        picker.SetActive(button.IsPressed());
+        /*picker.SetActive(button.IsPressed());*/
     }
 }
